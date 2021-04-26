7be23073e1dee529517b80e29d80bf13b3926dd1
mbenabda1992 <malik.benabda.lb@gmail.com>
2021-04-27 01:14:17 +0200

    list gen cleanup

--- src/main/java/de/tabit/test/alexandria/engine/dummy/FieldServices.java
+++ src/main/java/de/tabit/test/alexandria/engine/dummy/FieldServices.java
@@ -3,26 +3,19 @@ package de.tabit.test.alexandria.engine.dummy;
 import de.tabit.test.alexandria.entity.*;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class FieldServices implements PlayerControl {
 
     public static List<TrapField> generateTraps(Integer count) {
-        List<TrapField> traps = new ArrayList<>();
-        for (int i = 0; i < count; i++) {
-            // traps.add(new TrapField(Utilities.randomEnum(TrapType.class))) ;
-            traps.add(new TrapField());
-        }
-        return traps;
+        return new ArrayList<>(Collections.nCopies(count, new TrapField()));
     }
 
     public static List<BonusField> generateBonus(Integer bonusNum) {
-        List<BonusField> bonusList = new ArrayList<>(bonusNum);
-        for (int i = 0; i < bonusNum; i++) {
-            // bonusList.add(new BonusField(Utilities.randomEnum(BonusType.class))) ;
-            bonusList.add(new BonusField());
-        }
-        return bonusList;
+        return Stream.generate(BonusField::new).limit(bonusNum).collect(Collectors.toList());
     }
 
     public void doSpecialFieldAction(EngineData enginData, Player p) {
@@ -32,19 +25,19 @@ public class FieldServices implements PlayerControl {
         } else if (currentField instanceof BonusField) {
             handleBonus(currentField, p, enginData);
         } else {
-            System.out.println( p.getName().concat( " landed on a standard Field"));
+            System.out.println(p.getName().concat(" landed on a standard Field"));
         }
     }
 
     private void handleBonus(Field currentField, Player p, EngineData enginData) {
-        System.out.println( p.getName().concat( " landed on a BONUS Field"));
+        System.out.println(p.getName().concat(" landed on a BONUS Field"));
         switch (((BonusField) currentField).getType()) {
             case BONUS_TYPE1:
                 movePlayer(p, 2);
-                System.out.println( p.getName().concat( " advances by 2 Field"));
+                System.out.println(p.getName().concat(" advances by 2 Field"));
                 break;
             case BONUS_TYPE2: {
-                System.out.println( p.getName().concat( " pushes others 2 Fields back"));
+                System.out.println(p.getName().concat(" pushes others 2 Fields back"));
                 enginData.playerList.forEach(player -> {
                     if (!player.equals(p)) {
                         movePlayer(player, -2);
@@ -53,7 +46,7 @@ public class FieldServices implements PlayerControl {
                 break;
             }
             case BONUS_TYPE3:
-                System.out.println( p.getName().concat( " landed on a **JOKER**"));
+                System.out.println(p.getName().concat(" landed on a **JOKER**"));
                 p.setHasJoker(Boolean.TRUE);
                 break;
         }
@@ -61,12 +54,13 @@ public class FieldServices implements PlayerControl {
     }
 
     private void handleTraps(Field currentField, Player p, EngineData enginData) {
-        System.out.println( p.getName().concat( " landed on a TRAP Field"));
+        System.out.println(p.getName().concat(" landed on a TRAP Field"));
         switch (((TrapField) currentField).getType()) {
             case TRAP_TYPE1:
-                if ( p.getHasJoker()) p.setHasJoker(false);
-                else { movePlayer(p, -2);
-                    System.out.println( p.getName().concat( " was pushed back by a TRAP"));
+                if (p.getHasJoker()) p.setHasJoker(false);
+                else {
+                    movePlayer(p, -2);
+                    System.out.println(p.getName().concat(" was pushed back by a TRAP"));
                 }
                 break;
             case TRAP_TYPE2:
@@ -75,10 +69,10 @@ public class FieldServices implements PlayerControl {
                         movePlayer(player, 2);
                     }
                 });
-                System.out.println( p.getName().concat( "pushed everyone 2 Fields forward"));
+                System.out.println(p.getName().concat("pushed everyone 2 Fields forward"));
                 break;
             case TRAP_TYPE3:
-                System.out.println( p.getName().concat( " has to skip next turn"));
+                System.out.println(p.getName().concat(" has to skip next turn"));
                 p.setCuredSkip(true);
                 break;
         }
@@ -88,7 +82,7 @@ public class FieldServices implements PlayerControl {
     @Override
     public Integer movePlayer(Player p, Integer jumps) {
         Integer position = p.getPosition();
-        position =  Math.max(0, Math.min(DummyEngine.ROAD_END, position));
+        position = Math.max(0, Math.min(DummyEngine.ROAD_END, position));
         p.setPosition(position);
         return position;
     }
